7255cdb399e6839cc814f14e889e94ba3ab16793
Pavel <gamenimsi@gmail.com>
2024-01-23 17:06:20 +0300

    up

--- src/components/modals/members-modal.tsx
+++ src/components/modals/members-modal.tsx
@@ -10,23 +10,33 @@ import {
 import {
 	DropdownMenu,
 	DropdownMenuContent,
+	DropdownMenuItem,
 	DropdownMenuPortal,
+	DropdownMenuSeparator,
 	DropdownMenuSub,
 	DropdownMenuSubContent,
 	DropdownMenuSubTrigger,
 	DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import {
+	Check,
+	Gavel,
+	Loader2,
+	MoreVertical,
+	Shield,
+	ShieldAlert,
+	ShieldCheck,
+	ShieldQuestion,
+} from 'lucide-react'
 
 import { ScrollArea } from '@/components/ui/scroll-area'
 import { UserAvatar } from '@/components/user-avatar'
 import { useModal } from '@/hooks/use-modal-store'
 import { ServerWithMembersWithProfiles } from '@/types'
-import {
-	MoreVertical,
-	ShieldAlert,
-	ShieldCheck,
-	ShieldQuestion,
-} from 'lucide-react'
+import { MemberRole } from '@prisma/client'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import qs from 'query-string'
 import { useState } from 'react'
 
 const roleIconMap = {
@@ -36,11 +46,53 @@ const roleIconMap = {
 }
 
 export const MembersModal = () => {
+	const router = useRouter()
 	const { onOpen, isOpen, onClose, type, data } = useModal()
-	const [loadingId, setLoadingId] = useState()
+	const [loadingId, setLoadingId] = useState('')
 
 	const isModalOpen = isOpen && type === 'members'
 	const { server } = data as { server: ServerWithMembersWithProfiles }
+
+	const onKick = async (memberId: string) => {
+		try {
+			setLoadingId(memberId)
+			const url = qs.stringifyUrl({
+				url: `/api/members/${memberId}`,
+				query: {
+					serverId: server?.id,
+				},
+			})
+			const response = await axios.delete(url)
+
+			router.refresh()
+			onOpen('members', { server: response.data })
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setLoadingId('')
+		}
+	}
+
+	const onRoleChange = async (memberId: string, role: MemberRole) => {
+		try {
+			setLoadingId(memberId)
+			const url = qs.stringifyUrl({
+				url: `/api/members/${memberId}`,
+				query: {
+					serverId: server?.id,
+				},
+			})
+
+			const response = await axios.patch(url, { role })
+
+			router.refresh()
+			onOpen('members', { server: response.data })
+		} catch (error) {
+			console.log(error)
+		} finally {
+			setLoadingId('')
+		}
+	}
 
 	return (
 		<Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -78,13 +130,42 @@ export const MembersModal = () => {
 														<span>Роль</span>
 													</DropdownMenuSubTrigger>
 													<DropdownMenuPortal>
-														<DropdownMenuSubContent>312</DropdownMenuSubContent>
+														<DropdownMenuSubContent>
+															<DropdownMenuItem
+																onClick={() =>
+																	onRoleChange(member.id, 'MODERATOR')
+																}
+															>
+																<ShieldCheck className='h-4 w-4 mr-2' />
+																Модератор
+																{member.role === 'MODERATOR' && (
+																	<Check className='h-4 w-4 ml-auto' />
+																)}
+															</DropdownMenuItem>
+															<DropdownMenuItem
+																onClick={() => onRoleChange(member.id, 'GUEST')}
+															>
+																<Shield className='h-4 w-4 mr-2' />
+																Участник
+																{member.role === 'GUEST' && (
+																	<Check className='h-4 w-4 ml-auto' />
+																)}
+															</DropdownMenuItem>
+														</DropdownMenuSubContent>
 													</DropdownMenuPortal>
 												</DropdownMenuSub>
+												<DropdownMenuSeparator />
+												<DropdownMenuItem>
+													<Gavel className='w-4 h-4 mr-2' />
+													Выгнать
+												</DropdownMenuItem>
 											</DropdownMenuContent>
 										</DropdownMenu>
 									</div>
 								)}
+							{loadingId === member.id && (
+								<Loader2 className='animate-spin text-zinc-500 ml-auto w-4 h-4' />
+							)}
 						</div>
 					))}
 				</ScrollArea>
