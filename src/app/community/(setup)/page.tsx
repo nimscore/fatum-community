92f59ef3a89ecb7d0a02aad2cc5f357f6dc77d17
Pavel <gamenimsi@gmail.com>
2024-01-29 16:51:00 +0300

    up

--- src/app/community/(setup)/page.tsx
+++ src/app/community/(setup)/page.tsx
@@ -1,3 +1,5 @@
+import { ServerList } from '@/components/(community)/server/server-list'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import { initialProfile } from '@/lib/(profile)/initial-profile'
 import { db } from '@/lib/db'
 import { redirect } from 'next/navigation'
@@ -18,7 +20,24 @@ const WelcomeCommunityPage = async () => {
 		return redirect(`/community/servers/${server.id}`)
 	}
 	// return <InitialModal />
-	return null
+	const servers = await db.server.findMany()
+	return (
+		<>
+			<div className=''>
+				<ScrollArea className='flex-1 w-full'>
+					{servers.map(server => (
+						<div key={server.id} className='mb-4'>
+							<ServerList
+								id={server.id}
+								name={server.name}
+								imageUrl={server.imageUrl}
+							/>
+						</div>
+					))}
+				</ScrollArea>
+			</div>
+		</>
+	)
 }
 
 export default WelcomeCommunityPage
